d42109a7848fed9eed1b342dc83c9680acbad54c
Karan singh <155845482+Deltron18@users.noreply.github.com>
2025-08-28 13:27:46 +0530

    Time: 6 ms (26.81%) | Memory: 99.1 MB (9.63%) - LeetSync

--- 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
+++ 2394-count-subarrays-with-score-less-than-k/count-subarrays-with-score-less-than-k.cpp
@@ -1,19 +1,42 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, long long k) {
-        long long start = 0, sum = 0, count = 0;
-        for (long long end = 0; end < nums.size(); end++) {
-            sum += nums[end];
-
-            // shrink window if score >= k
-            while (sum * (end - start + 1) >= k) {
-                sum -= nums[start];
+        long long start=0,end=0,sum=0,count=0;
+        while(end<nums.size()){
+            sum+=nums[end];
+            while(sum*(end-start+1)>=k){
+                sum-=nums[start];
                 start++;
             }
-
-            // number of subarrays ending at 'end' = window length
-            count += (end - start + 1);
+            
+            count+=(end-start+1);
+            end++;
         }
         return count;
+
     }
 };
+
+//   long long countSubarrays(vector<int>& nums, long long k) {
+//         int n = nums.size();
+//         int i=0,j=0;
+//         long long sum = 0;
+//         long long cnt = 0;
+        
+
+//         while(j<n){
+//             sum += nums[j];
+
+//             while((long long)sum * (j-i+1) >= k){
+//                 //shrink 
+//                 sum -= nums[i];
+//                 i++;
+//             }
+
+//             cnt += (j-i+1);
+//             j++;
+//         }
+
+//         return cnt;
+//     }
+// };
