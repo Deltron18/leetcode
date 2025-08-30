d5b003a00906fae599cdea284cb7dfc8e548ccc5
Karan singh <155845482+Deltron18@users.noreply.github.com>
2025-08-30 12:03:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (73.54%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        // if(n<=1)
+        // return 1;
+
+        // return climbStairs(n-1)+climbStairs(n-2);
+
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+
+        int arr[100000];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+
+        return arr[n];
+        
+    }
+};
