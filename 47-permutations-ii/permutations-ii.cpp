d707ef7b1f838cc0270fc6a1732c5647e6c40aa4
Karan singh <155845482+Deltron18@users.noreply.github.com>
2025-09-03 14:22:50 +0530

    Time: 6 ms (45.77%) | Memory: 11.8 MB (66.67%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -11,7 +11,7 @@ if(index==nums.size()){
   vector<bool>check(21,0);
   for(int i=index;i<nums.size();i++){
     if(check[nums[i]+10]==0){
-        swap(nums[index],nums[i]);
+        swap(nums[index],nums[i]);  //little doubt
         permute(nums,ans,index+1);
         swap(nums[index],nums[i]);
         check[nums[i]+10]=1;
