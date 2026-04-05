339377607c940c8724b392cdc67a6eb10301635e
Karan singh <155845482+Deltron18@users.noreply.github.com>
2026-04-05 12:35:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,26 +11,41 @@
 
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
         // dummy node to simplify logic
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
+        // ListNode dummy(0);
+        // ListNode* tail = &dummy;
 
-        while (list1 && list2) {
-            if (list1->val < list2->val) {
-                tail->next = list1;
-                list1 = list1->next;
-            } else {
-                tail->next = list2;
-                list2 = list2->next;
-            }
-            tail = tail->next;
+        // while (list1 && list2) {
+        //     if (list1->val < list2->val) {
+        //         tail->next = list1;
+        //         list1 = list1->next;
+        //     } else {
+        //         tail->next = list2;
+        //         list2 = list2->next;
+        //     }
+        //     tail = tail->next;
+        // }
+
+        // // append the remaining nodes
+        // if (list1) tail->next = list1;
+        // else tail->next = list2;
+
+        // return dummy.next;
+
+
+        //using recursion
+        if(l1==NULL || l2==NULL)
+        return l1 == NULL ? l2 : l1;
+
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+        else{
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
         }
 
-        // append the remaining nodes
-        if (list1) tail->next = list1;
-        else tail->next = list2;
-
-        return dummy.next;
     }
 };
