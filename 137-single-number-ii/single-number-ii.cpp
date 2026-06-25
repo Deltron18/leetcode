579e0b36004112613e3323668893c6f6cc7cdd74
Karan singh <155845482+Deltron18@users.noreply.github.com>
2026-06-26 00:53:41 +0530

    Time: 1 ms (42.82%) | Memory: 13.5 MB (23.33%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -3,20 +3,20 @@ public:
     int singleNumber(vector<int>& nums) {
         if(nums.size()<2)
         return nums[0];
-         int first,third,fourth;
+        int ans;
         sort(nums.begin(),nums.end());
-        int i=0;
+    int i=0;
         while(i<=nums.size()-4){
-        first=nums[i];
-        third=nums[i+2];
-        fourth=nums[i+3];
-
-        if(first!=third)
-        return first;
-       
-       i+=3;
-
+            if(nums[i]==nums[i+2])
+           {
+             ans=nums[i+3];
+              i=i+3;
+          
+           }
+        else{
+            return nums[i];
         }
-    return fourth;
+        }
+return ans;
     }
 };
