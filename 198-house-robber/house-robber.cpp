d474ec6084c55a577faf157841cd7fbfab475464
Karan singh <155845482+Deltron18@users.noreply.github.com>
2026-09-21 17:00:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (23.39%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,34 +1,48 @@
-class Solution {
-public:
-
-int robber(int i,vector<int>& nums,vector<int>& dp){
-    if(i==0)
-    return nums[0];
-
-    if(i==1)
-    return max(nums[0],nums[1]);
-
-    if(dp[i]!=-1)
-    return dp[i];
-
-    return dp[i]=max((nums[i]+robber(i-2,nums,dp)),robber(i-1,nums,dp));
-}
-
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        int i;
-        vector<int>dp(n,-1);
-        return robber(n-1,nums,dp);
-    }
-};
-
-
-
-
+// top down approachh
 
 // class Solution {
 // public:
+
+// int robber(int i,vector<int>& nums,vector<int>& dp){
+//     if(i==0)
+//     return nums[0];
+
+//     if(i==1)
+//     return max(nums[0],nums[1]);
+
+//     if(dp[i]!=-1)
+//     return dp[i];
+
+//     return dp[i]=max((nums[i]+robber(i-2,nums,dp)),robber(i-1,nums,dp));
+// }
+
 //     int rob(vector<int>& nums) {
-        
+//         int n=nums.size();
+//         int i;
+//         vector<int>dp(n,-1);
+//         return robber(n-1,nums,dp);
 //     }
 // };
+
+
+
+
+// bottom up approach
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if (n == 1)
+            return nums[0];
+        vector<int>dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<n;i++){
+            dp[i]=max((nums[i]+dp[i-2]),dp[i-1]);
+        }
+
+        return dp[n-1];
+    }
+};
